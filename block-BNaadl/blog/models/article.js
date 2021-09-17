3364963bf2aac260f9e2689868553245e8a38699
Aman Thakur <uttamthakur2999@gmail.com>
2021-09-17 10:51:35 +0530

    Assigment-1

---
+++ block-BNaadl/blog/models/article.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+let articleSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    tags: [String],
+    author: String,
+    likes: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+let Article = mongoose.model('Article', articleSchema);
+module.exports = Article;
